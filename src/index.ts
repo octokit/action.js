f5adbac13fea3ee9074e7e0eb60bdc9c58771c63
Gregor Martynus <gregor@martynus.net>
2019-10-07 12:46:19 -0700

    feat: pre-authenticationd [`@octokit/core`](https://github.com/octokit/core.js) instance

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Octokit as Core } from "@octokit/core";
 import { createActionAuth } from "@octokit/auth-action";
 
+import { VERSION } from "./version";
+
 export const Octokit = Core.defaults({
-  auth: createActionAuth()
+  auth: createActionAuth(),
+  userAgent: `octokit-action.js/${VERSION}`
 });
