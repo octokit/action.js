8146777e553c5e81d5f160601c129d69232459c8
Gregor Martynus <gregor@martynus.net>
2019-11-19 15:15:38 -0800

    feat: REST endpoint methods

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { Octokit as Core } from "@octokit/core";
 import { createActionAuth } from "@octokit/auth-action";
 import { paginateRest } from "@octokit/plugin-paginate-rest";
+import { restEndpointMethods } from "@octokit/plugin-rest-endpoint-methods";
 
 import { VERSION } from "./version";
 
-export const Octokit = Core.plugin([paginateRest]).defaults({
+export const Octokit = Core.plugin([
+  paginateRest,
+  restEndpointMethods
+]).defaults({
   authStrategy: createActionAuth,
   userAgent: `octokit-action.js/${VERSION}`
 });
