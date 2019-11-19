b84202707f86609297163869a716439abd8fcf23
Gregor Martynus <gregor@martynus.net>
2019-11-19 15:14:15 -0800

    test: REST endpoint methods

--- test/smoke.test.ts
+++ test/smoke.test.ts
@@ -1,3 +1,5 @@
+import fetchMock from "fetch-mock";
+
 import { Octokit } from "../src";
 
 describe("Smoke test", () => {
@@ -39,5 +41,109 @@ describe("Smoke test", () => {
     const octokit = new MyOctokit();
     expect(typeof octokit.paginate).toBe("function");
     expect(octokit.foo).toBe("bar");
+  });
+
+  it("README example: create issue using octokit.request()", async () => {
+    process.env.GITHUB_TOKEN = "secret123";
+    process.env.GITHUB_ACTION = "test";
+
+    const mock = fetchMock.sandbox().post(
+      "path:/repos/octocat/hello-world/issues",
+      { ok: true },
+      {
+        body: {
+          title: "My test issue"
+        }
+      }
+    );
+
+    const octokit = new Octokit({
+      auth: "secret123",
+      request: {
+        fetch: mock
+      }
+    });
+
+    // See https://developer.github.com/v3/issues/#create-an-issue
+    const { data } = await octokit.request("POST /repos/:owner/:repo/issues", {
+      owner: "octocat",
+      repo: "hello-world",
+      title: "My test issue"
+    });
+
+    expect(data).toStrictEqual({ ok: true });
+  });
+
+  it("README example: create issue using octokit.issues.create()", async () => {
+    process.env.GITHUB_TOKEN = "secret123";
+    process.env.GITHUB_ACTION = "test";
+
+    const mock = fetchMock.sandbox().post(
+      "path:/repos/octocat/hello-world/issues",
+      { ok: true },
+      {
+        body: {
+          title: "My test issue"
+        }
+      }
+    );
+
+    const octokit = new Octokit({
+      auth: "secret123",
+      request: {
+        fetch: mock
+      }
+    });
+
+    // See https://developer.github.com/v3/issues/#create-an-issue
+    const { data } = await octokit.issues.create({
+      owner: "octocat",
+      repo: "hello-world",
+      title: "My test issue"
+    });
+
+    expect(data).toStrictEqual({ ok: true });
+  });
+
+  it("README example: create issue using octokit.graphql", async () => {
+    process.env.GITHUB_TOKEN = "secret123";
+    process.env.GITHUB_ACTION = "test";
+
+    const query = `mutation($repositoryId:ID!, $title:String!) {
+      createIssue(input:{repositoryId: $repositoryId, title: $title}) {
+        issue {
+          number
+        }
+      }
+    }`;
+
+    const mock = fetchMock.sandbox().post(
+      "path:/graphql",
+      { data: { ok: true } },
+      {
+        body: {
+          query,
+          variables: {
+            repositoryId: 123,
+            title: "My test issue"
+          }
+        }
+      }
+    );
+
+    const octokit = new Octokit({
+      auth: "secret123",
+      request: {
+        fetch: mock
+      }
+    });
+
+    // See https://developer.github.com/v3/issues/#create-an-issue
+    const data = await octokit.graphql(query, {
+      repositoryId: 123,
+      title: "My test issue"
+    });
+
+    expect(data).toStrictEqual({ ok: true });
   });
 });
