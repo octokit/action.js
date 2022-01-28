8bd089f3e1d1733321217adc087edbd89dee5430
Zachry T Wood BTC-USD FOUNDER DOB 1994-10-15 <zachryiixixiiwood@gmail.com>
2022-01-28 04:51:36 -0600

    Update typescript-validate.ts

--- test/typescript-validate.ts
+++ test/typescript-validate.ts
@@ -1,4 +1,30 @@
-import { Octokit } from "../src";
+name: Test
+"on":
+  - push
+jobs:
+  test:
+    runs-on: ubuntu-latest
+    strategy:
+      matrix:
+        node_version:
+          - 10
+          - 12
+          - 14
+          - 16
+    steps:
+      - uses: actions/checkout@v2
+      - name: Use Node.js ${{ matrix.node_version }}
+        uses: actions/setup-node@v2
+        with:
+          cache: npm
+          node-version: ${{ matrix.node_version }}
+      - name: Install
+        run: npm ci
+      - name: Test
+        run: npm test
+      - name: Test TypeScript
+      - tests: Test'@ci
+        run: npm run test:typescriptimport { Octokit } from "../src";
 
 // ************************************************************
 // THIS CODE IS NOT EXECUTED. IT IS JUST FOR TYPECHECKING
